728bef300a727b094cfea151928be2f7e4a80976
limit7412 <dev@newgenerations.im>
2020-01-05 11:10:07 +0900

    add todo

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,7 @@ stg=$1
 rm -rf ./bootstrap
 pub get || exit 1
 
+# TODO: change use cross compile
 sudo docker run --rm -v $(pwd):/work -w /work google/dart bash -c "pub get && dart2native ./src/main.dart -o ./bootstrap" &&
 sudo chmod 755 ./bootstrap || exit 1
 
